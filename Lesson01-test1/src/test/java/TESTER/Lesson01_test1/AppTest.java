b20e6361b84019e170e67e48ffacd6379174d33d
Thao Ly <thaolydn1805@gmail.com>
2021-08-22 17:58:50 +0700

    lesson01 -22/08

---
+++ Lesson01-test1/src/test/java/TESTER/Lesson01_test1/AppTest.java
@@ -0,0 +1,20 @@
+package TESTER.Lesson01_test1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
